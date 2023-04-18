3f0c622d3b74504bdaa47357b181f896f57640af
MauroLibrera <maurolibrera@gmail.com>
2023-04-18 20:49:05 -0300

    Folder Change

---
+++ Intro Aerodynamics/Joukowsky/sources/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    float x, y;
+
+    std::cout << "Escribir x origen ";
+    std::cin >> x;
+
+    std::cout << "Escribir y origen ";
+    std::cin >> y;
+    
+
+    return 0;
+}
