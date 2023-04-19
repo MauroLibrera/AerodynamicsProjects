ab990baf924052cce927058fdf59f95938c3c775
MauroLibrera <maurolibrera@gmail.com>
2023-04-19 17:26:42 -0300

    Re do

--- Intro Aerodynamics/Joukowsky/headers/joukowsky.hpp
+++ Intro Aerodynamics/Joukowsky/headers/joukowsky.hpp
@@ -1,30 +1,14 @@
 #ifndef JOUKOWSKY_H
 #define JOUKOWSKY_H
 
-#include "circunferencia.hpp"
-#include "transformada.hpp"
-
 
 class Joukowsky
 {
 public:
     Joukowsky(float x, float y);
 
-    float x0;
-    float y0;
-    int n;
-
-    void Ejecutar();
-
 private:
-    Circunferencia circ;
-    Transformada* transform;
 
-    float* xc;
-    float* yc;
-
-    float* xp;
-    float* yp;
 };
 
 
