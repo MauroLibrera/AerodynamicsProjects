5c1d6c88367f526ae2213fbd92c7124e1e650ab1
MauroLibrera <maurolibrera@gmail.com>
2023-04-19 17:22:45 -0300

    FInished definition for the airfoils

--- Intro Aerodynamics/Joukowsky/sources/perfil.cpp
+++ Intro Aerodynamics/Joukowsky/sources/perfil.cpp
@@ -11,8 +11,10 @@ void perfil::CalculoPuntos()
     std::cout << "x " << " y" << std::endl;
     for (int i=0; i<N; i++)
     {
-        x[i] = circBase.GetX()[i];
-        y[i] = circBase.GetY()[i];
+        x[i] = circBase.GetX()[i] + circBase.GetX()[i]/(circBase.GetX()[i]*circBase.GetX()[i] + 
+        circBase.GetY()[i]*circBase.GetY()[i]);
+        y[i] = circBase.GetY()[i] - circBase.GetY()[i]/(circBase.GetX()[i]*circBase.GetX()[i] + 
+        circBase.GetY()[i]*circBase.GetY()[i]);
 
         std::cout << x[i] << " " << y[i] << std::endl;
     }
