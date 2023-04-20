a676a9dc3f69a251f03bb034cc876fa9ff3dd54c
MauroLibrera <maurolibrera@gmail.com>
2023-04-20 19:52:30 -0300

    Clean

--- Intro Aerodynamics/Joukowsky/sources/main.cpp
+++ Intro Aerodynamics/Joukowsky/sources/main.cpp
@@ -1,26 +1,8 @@
 #include <iostream>
-#include "../headers/circunferencia.hpp"
-#include "../headers/perfil.hpp"
 #include "../headers/JouApp.hpp"
 
 int main(int argc, char* argv[])
 {
-    /***
-    float x, y;
-
-    std::cout << "Escribir x origen ";
-    std::cin >> x;
-
-    std::cout << "Escribir y origen ";
-    std::cin >> y;
-    
-    Circunferencia circ = Circunferencia(x, y);
-    circ.CalculoPuntos();
-
-    perfil per = perfil(circ);
-    per.CalculoPuntos();
-    ***/
-
    JouApp app;
 
     return app.Ejecutar();
